b3387301070c303d90c0b03f53f86aa0783d6db0
Bismark Kwaku Kete <kbismarkkwaku@gmail.com>
2025-11-07 09:56:05 +0000

    Debugged the issue where I was getting a float instead of an integer.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,7 +4,7 @@ income = int(input("Enter your monthly income:"))
 expenses = int(input("Enter your total monthly expenses:"))
 
 monthly_savings = income - expenses
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+annual_savings = int((monthly_savings * 12 + (monthly_savings * 12 * 0.05)))
 
 print("Your monthly savings are ${}.".format(monthly_savings))
 print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
