2b27a06e38137952a676767ec32694114bdce08c
Bismark Kwaku Kete <kbismarkkwaku@gmail.com>
2025-11-12 10:13:42 +0000

    Resolved the checkers error

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,34 +8,28 @@ time_bound = input("Is it time-bound? (yes or no): ")
 
 
 # processing the task based on priority and time sensitivity
-def main():
-    match priority:
-        case "high":
-            string = f"Reminder: '{task}' is a {priority} priority task"
-            address(string)
-        case "medium":
-            string = f"Reminder: '{task}' is a {priority} priority task"
-            address(string)
-        case "low":
-            string = f"Reminder: '{task}' is a {priority} priority task"
-            address(string)
-        case _:
-            no_priority()
+match priority:
+    case "high":
+        string = f"Reminder: '{task}' is a {priority} priority task"
+        address(string)
+    case "medium":
+        string = f"Reminder: '{task}' is a {priority} priority task"
+        address(string)
+    case "low":
+        string = f"Reminder: '{task}' is a {priority} priority task"
+        address(string)
+    case _:
+        no_priority()
 
 
 # check if the reminder is time-bound and address the user
-def address(string):
-    if time_bound == "yes":
-        print(f"{string} that requires your immediate attention today.")
-    elif time_bound == "no":
-        print(f"{string}. Consider completing it when you have free time.")
-    else:
-        print("Incorrect time-bound type selected. Enjoy!")
+if time_bound == "yes":
+    print(f"{string} that requires your immediate attention today.")
+elif time_bound == "no":
+    print(f"{string}. Consider completing it when you have free time.")
+else:
+    print("Incorrect time-bound type selected. Enjoy!")
 
 
 # function to handle a user typing a different priority
-def no_priority():
-    print("The priority you typed doesn't exist in the software.")
-
-
-main()
+print("The priority you typed doesn't exist in the software.")
